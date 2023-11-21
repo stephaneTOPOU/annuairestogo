853c6fc4d57e143f7c7c4b24472d2087ed8d824a
Hugues658 <tekoaffohugues@gmail.com>
2023-11-21 17:23:53 +0000

    commit

--- resources/views/frontend/jobdetail.blade.php
+++ resources/views/frontend/jobdetail.blade.php
@@ -38,18 +38,19 @@
 		<section class="sptb">
 			<div class="container">
 				<div class="row">
+					@foreach ($offres_details as $details)
 					<div class="col-xl-8 col-lg-8 col-md-12">
 
 						<!--Classified Description-->
 						<div class="card overflow-hidden">
 							<div class="card-body h-100 boot-slider">
 								<div class="item-det mb-4">
-									<h3 >Maruti Suzuki Alto</h3></a>
+									<h3 >{{$details->titre}}</h3></a>
 									<div class=" d-flex">
 										<ul class="d-flex mb-0">
-											<li class="me-5"><i class="icon icon-briefcase text-muted me-1"></i> Cars</li>
-											<li class="me-5"><i class="icon icon-location-pin text-muted me-1"></i> USA</li>
-											<li class="me-5"><i class="icon icon-calendar text-muted me-1"></i> 5 hours ago</li>
+											<li class="me-5"><i class="icon icon-briefcase text-muted me-1"></i> {{$details->entreprise}}</li>
+											<li class="me-5"><i class="icon icon-location-pin text-muted me-1"></i>{{$details->ville}}</li>
+											<li class="me-5"><i class="icon icon-calendar text-muted me-1"></i> {{$details->created_at}}</li>
 										</ul>
 									</div>
 								</div>
@@ -62,8 +63,7 @@
 							</div>
 							<div class="card-body">
 								<div class="mb-4">
-									<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atcorrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
-									<p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoraliz the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble thena bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.</p>
+									<p>{!! $details->description !!}</p>
 								</div>
 							</div>
 							<div class="card-header">
@@ -71,8 +71,7 @@
 							</div>
 							<div class="card-body">
 								<div class="mb-4">
-									<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atcorrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
-									<p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoraliz the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble thena bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.</p>
+									<p>{!! $details->mission !!}</p>
 								</div>
 							</div>
 							<div class="card-header">
@@ -80,290 +79,69 @@
 							</div>
 							<div class="card-body">
 								<div class="mb-4">
-									<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atcorrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.</p>
-									<p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoraliz the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble thena bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.</p>
+									<p>{!! $details->profil !!}</p>
 								</div>
 							</div>
 							<div class="pt-4 pb-4 px-5 border-top border-top">
 								<div class="list-id">
 									<div class="row">
 										<div class="col">
-											<a class="mb-0">Classified ID : #8256358</a>
+											<a class="mb-0">Lien : {{$details->lien}}</a>
 										</div>
 										<div class="col col-auto">
-											Posted By <a class="mb-0 font-weight-bold">Individual</a> / 21st Dec 2018
+											<a class="mb-0 font-weight-bold">Date limite : {{$details->date_lim}}</a>
 										</div>
 									</div>
 								</div>
 							</div>
 							<div class="card-footer">
 								<div class="icons">
-									<a href="#" class="btn btn-info icons"><i class="icon icon-share me-1"></i> Share Ad</a>
-									<a href="#" class="btn btn-secondary icons"><i class="icon icon-printer  me-1"></i> Print</a>
+									<a href="#" class="btn btn-info icons"><i class="icon icon-share me-1"></i> Partager</a>
+									<a href="#" class="btn btn-secondary icons"><i class="icon icon-printer  me-1"></i> Imprimer</a>
 								</div>
 							</div>
 						</div>
 
-						<div class="card mb-lg-0">
-							<div class="card-header">
-								<h3 class="card-title">Leave a reply</h3>
-							</div>
-							<div class="card-body">
-								<div>
-									<div class="form-group">
-										<input type="text" class="form-control" id="name1" placeholder="Your Name">
-									</div>
-									<div class="form-group">
-										<input type="email" class="form-control" id="email" placeholder="Email Address">
-									</div>
-									<div class="form-group">
-										<div class="form-file">
-											<input type="file" class="form-control example-file-input-custom" name="example-file-input-custom">
-										</div>
-									</div>
-									<div class="form-group">
-										<textarea class="form-control" name="example-textarea-input" rows="6" placeholder="Comment"></textarea>
-									</div>
-									<a href="#" class="btn btn-primary">Send Reply</a>
-								</div>
-							</div>
-						</div>
 						<!--/Classified Description-->
 
-						<h3 class="mb-5 mt-4">Related Posts</h3>
+						<h3 class="mb-5 mt-4">Annonces récentes</h3>
 
 						<!--Related Posts-->
 						<div id="myCarousel5" class="owl-carousel owl-carousel-icons3">
 							<!-- Wrapper for carousel items -->
-
+							@foreach ($offres as $offre)
 							<div class="item">
 								<div class="card">
-									<div class="ribbon ribbon-top-left text-danger"><span class="bg-danger">Negotiable</span></div>
 									<div class="item-card7-imgs">
-										<a href="classified.html"></a>
-										<img src="../assets/images/products/products/ed1.jpg" alt="img" class="cover-image">
+										<a href="{{ route('offre.detail',['slug_pays'=>'tg','offre_slug'=>$offre->slug_offres])}}"></a>
+										<img src="../assets/images/products/products/job.jpg" alt="img" class="cover-image">
 									</div>
 									<div class="item-card7-overlaytext">
-										<a href="classified.html" class="text-white"> Education</a>
-										<h4  class="font-weight-semibold mb-0">$389</h4>
+										<a class="text-white"> {{$offre->categorie}}</a>
 									</div>
 									<div class="card-body">
 										<div class="item-card7-desc">
-											<a href="classified.html" class="text-dark"><h4 class="font-weight-semibold">Record Writing</h4></a>
+											<a href="{{ route('offre.detail',['slug_pays'=>'tg','offre_slug'=>$offre->slug_offres])}}" class="text-dark"><h4 class="font-weight-semibold">{{$offre->titre}}</h4></a>
 										</div>
 										<div class="item-card7-text">
 											<ul class="icon-card mb-0">
-												<li ><a href="#" class="icons"><i class="icon icon-location-pin text-muted me-1"></i>  Los Angles</a></li>
-												<li><a href="#" class="icons"><i class="icon icon-event text-muted me-1"></i> 5 hours ago</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-user text-muted me-1"></i> Sally Peake</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-phone text-muted me-1"></i> 5-67987608</a></li>
+												<li ><a class="icons"><i class="icon icon-briefcase text-muted me-1"></i>  {{$offre->entreprise}}</a></li>
+												<li><a class="icons"><i class="icon icon-event text-muted me-1"></i> {{$offre->created_at}}</a></li>
+												<li class="mb-0"><a class="icons"><i class="icon icon-location-pin text-muted me-1"></i> {{$offre->ville}}</a></li>
+												<li class="mb-0"><a class="icons"><i class="icon icon-phone text-muted me-1"></i> {{$offre->telephone}}</a></li>
 											</ul>
-											<p class="mb-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem....</p>
+											<p class="mb-0">{!! $offre->description !!}</p>
 										</div>
 									</div>
 								</div>
 							</div>
-							<div class="item">
-								<div class="card">
-									<div class="ribbon ribbon-top-left text-primary"><span class="bg-primary">featured</span></div>
-									<div class="item-card7-imgs">
-										<a href="classified.html"></a>
-										<img src="../assets/images/products/products/v1.jpg" alt="img" class="cover-image">
-									</div>
-									<div class="item-card7-overlaytext">
-										<a href="classified.html" class="text-white"> Cars</a>
-										<h4  class=" mb-0">$854</h4>
-									</div>
-									<div class="card-body">
-										<div class="item-card7-desc">
-											<a href="classified.html" class="text-dark"><h4 class="font-weight-semibold">Geep Automobiles</h4></a>
-										</div>
-										<div class="item-card7-text">
-											<ul class="icon-card mb-0">
-												<li ><a href="#" class="icons"><i class="icon icon-location-pin text-muted me-1"></i>  Los Angles</a></li>
-												<li><a href="#" class="icons"><i class="icon icon-event text-muted me-1"></i> 5 hours ago</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-user text-muted me-1"></i> Sally Peake</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-phone text-muted me-1"></i> 5-67987608</a></li>
-											</ul>
-											<p class="mb-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem....</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="item">
-								<div class="card">
-									<div class="item-card7-img">
-										<div class="item-card7-imgs">
-											<a href="classified.html"></a>
-											<img src="../assets/images/products/products/j1.jpg" alt="img" class="cover-image">
-										</div>
-										<div class="item-card7-overlaytext">
-											<a href="classified.html" class="text-white">Furniture</a>
-											<h4  class="font-weight-semibold mb-0">$786</h4>
-										</div>
-									</div>
-									<div class="card-body">
-										<div class="item-card7-desc">
-											<a href="classified.html" class="text-dark"><h4 class="font-weight-semibold">Marketing Executive</h4></a>
-										</div>
-										<div class="item-card7-text">
-											<ul class="icon-card mb-0">
-												<li ><a href="#" class="icons"><i class="icon icon-location-pin text-muted me-1"></i>  Los Angles</a></li>
-												<li><a href="#" class="icons"><i class="icon icon-event text-muted me-1"></i> 5 hours ago</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-user text-muted me-1"></i> Sally Peake</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-phone text-muted me-1"></i> 5-67987608</a></li>
-											</ul>
-											<p class="mb-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem....</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="item">
-								<div class="card">
-									<div class="ribbon ribbon-top-left text-danger"><span class="bg-danger">Collection</span></div>
-									<div class="item-card7-img">
-										<div class="item-card7-imgs">
-											<a href="classified.html"></a>
-											<img src="../assets/images/products/products/f4.jpg" alt="img" class="cover-image">
-										</div>
-										<div class="item-card7-overlaytext">
-											<a href="classified.html" class="text-white"> Restaurant</a>
-											<h4  class="font-weight-semibold mb-0">$539</h4>
-										</div>
-									</div>
-									<div class="card-body">
-										<div class="item-card7-desc">
-											<a href="rclassified.html" class="text-dark"><h4 class="font-weight-semibold">Houge Restaurant</h4></a>
-										</div>
-										<div class="item-card7-text">
-											<ul class="icon-card mb-0">
-												<li ><a href="#" class="icons"><i class="icon icon-location-pin text-muted me-1"></i>  Los Angles</a></li>
-												<li><a href="#" class="icons"><i class="icon icon-event text-muted me-1"></i> 5 hours ago</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-user text-muted me-1"></i> Sally Peake</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-phone text-muted me-1"></i> 5-67987608</a></li>
-											</ul>
-											<p class="mb-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem....</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="item">
-								<div class="card">
-									<div class="item-card7-img">
-										<div class="item-card7-imgs">
-											<a href="classified.html"></a>
-											<img src="../assets/images/products/products/pe1.jpg" alt="img" class="cover-image">
-										</div>
-										<div class="item-card7-overlaytext">
-											<a href="classified.html" class="text-white"> Pets & Animals</a>
-											<h4  class="font-weight-semibold mb-0">$925</h4>
-										</div>
-									</div>
-									<div class="card-body">
-										<div class="item-card7-desc">
-											<a href="classified.html" class="text-dark"><h4 class="font-weight-semibold">Care Brohzm</h4></a>
-										</div>
-										<div class="item-card7-text">
-											<ul class="icon-card mb-0">
-												<li ><a href="#" class="icons"><i class="icon icon-location-pin text-muted me-1"></i>  Los Angles</a></li>
-												<li><a href="#" class="icons"><i class="icon icon-event text-muted me-1"></i> 5 hours ago</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-user text-muted me-1"></i> Sally Peake</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-phone text-muted me-1"></i> 5-67987608</a></li>
-											</ul>
-											<p class="mb-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem....</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="item">
-								<div class="card">
-									<div class="ribbon ribbon-top-left text-success"><span class="bg-success">Free Shipping</span></div>
-									<div class="item-card7-img">
-										<div class="item-card7-imgs">
-											<a href="classified.html"></a>
-											<img src="../assets/images/products/products/h5.jpg" alt="img" class="cover-image">
-										</div>
-										<div class="item-card7-overlaytext">
-											<a href="classified.html" class="text-white"> Homes</a>
-											<h4  class="font-weight-semibold mb-0">$925</h4>
-										</div>
-									</div>
-									<div class="card-body">
-										<div class="item-card7-desc">
-											<a href="classified.html" class="text-dark"><h4 class="font-weight-semibold"> Single Flat House</h4></a>
-										</div>
-										<div class="item-card7-text">
-											<ul class="icon-card mb-0">
-												<li ><a href="#" class="icons"><i class="icon icon-location-pin text-muted me-1"></i>  Los Angles</a></li>
-												<li><a href="#" class="icons"><i class="icon icon-event text-muted me-1"></i> 5 hours ago</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-user text-muted me-1"></i> Sally Peake</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-phone text-muted me-1"></i> 5-67987608</a></li>
-											</ul>
-											<p class="mb-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem....</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="item">
-								<div class="card">
-									<div class="item-card7-img">
-										<div class="item-card7-imgs">
-											<a href="classified.html"></a>
-											<img src="../assets/images/products/products/ed2.jpg" alt="img" class="cover-image">
-										</div>
-										<div class="item-card7-overlaytext">
-											<a href="classified.html" class="text-white"> Education</a>
-											<h4  class="font-weight-semibold mb-0">$378</h4>
-										</div>
-									</div>
-									<div class="card-body">
-										<div class="item-card7-desc">
-											<a href="classified.html" class="text-dark"><h4 class="font-weight-semibold">Digital Marketing Training </h4></a>
-										</div>
-										<div class="item-card7-text">
-											<ul class="icon-card mb-0">
-												<li ><a href="#" class="icons"><i class="icon icon-location-pin text-muted me-1"></i>  Los Angles</a></li>
-												<li><a href="#" class="icons"><i class="icon icon-event text-muted me-1"></i> 5 hours ago</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-user text-muted me-1"></i> Sally Peake</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-phone text-muted me-1"></i> 5-67987608</a></li>
-											</ul>
-											<p class="mb-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem....</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="item">
-								<div class="card">
-									<div class="ribbon ribbon-top-left text-primary"><span class="bg-primary">featured</span></div>
-									<div class="item-card7-img">
-										<div class="item-card7-imgs">
-											<a href="classified.html"></a>
-											<img src="../assets/images/products/products/j3.jpg" alt="img" class="cover-image">
-										</div>
-										<div class="item-card7-overlaytext">
-											<a href="classified.html" class="text-white">Spa</a>
-											<h4  class="font-weight-semibold mb-0">$836</h4>
-										</div>
-									</div>
-									<div class="card-body">
-										<div class="item-card7-desc">
-											<a href="classified.html" class="text-dark"><h4 class="font-weight-semibold">Designers</h4></a>
-										</div>
-										<div class="item-card7-text">
-											<ul class="icon-card mb-0">
-												<li ><a href="#" class="icons"><i class="icon icon-location-pin text-muted me-1"></i>  Los Angles</a></li>
-												<li><a href="#" class="icons"><i class="icon icon-event text-muted me-1"></i> 5 hours ago</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-user text-muted me-1"></i> Sally Peake</a></li>
-												<li class="mb-0"><a href="#" class="icons"><i class="icon icon-phone text-muted me-1"></i> 5-67987608</a></li>
-											</ul>
-											<p class="mb-0">Sed ut perspiciatis unde omnis iste natus error sit voluptatem....</p>
-										</div>
-									</div>
-								</div>
-							</div>
+							@endforeach
+							
 						</div>
 						<!--/Related Posts-->
 					</div>
+					@endforeach
+					
 
 					<!--Right Side Content-->
 					<div class="col-xl-4 col-lg-4 col-md-12">
